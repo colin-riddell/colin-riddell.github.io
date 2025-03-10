9b6710a455a934e1415045928aba836631a8ce58
Colin J Riddell <colinjohnriddell@gmail.com>
2025-03-10 23:05:51 +0000

    New blog

---
+++ _next/static/chunks/app/blog/page-c1d002509a276256.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404],{},_=>{var e=e=>_(_.s=e);_.O(0,[231,183,130,215,744],()=>e(6183)),_N_E=_.O()}]);
---
+++ _next/static/chunks/app/projects/page-acc1a8c0cc894482.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[895],{2881:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,231,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[231,173,130,215,744],()=>s(2881)),_N_E=e.O()}]);
